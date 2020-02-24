a9bd6a69c57458750ac58fbe4438301bf95365ec
Harrison Van <harrison.van@shopback.com>
2020-02-25 06:24:56 +0700

    fmt script

--- scripts/fmt-go.sh
+++ scripts/fmt-go.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
-set -eoux pipefail
+set -eou pipefail
 
-go fmt ./...
+mapfile -t FILES < <(git ls-files | grep --extended-regexp '.*\.(go)$')
+
+is_ignored() {
+  local -r file=$1
+
+  if [[ $file =~ ^unorganized\-.*\/.*$ ]]; then
+    echo -en "\e[33m"
+    echo "Skipping: $file"
+    echo -en "\e[0m"
+
+    return 0
+  fi
+
+  return 1
+}
+
+process() {
+  local -r file=$1
+
+  echo -en "\e[33m"
+  echo "Processing: $file"
+  echo -en "\e[0m"
+
+  go fmt "$file"
+}
+
+for file in "${FILES[@]}"; do
+  if is_ignored "$file"; then
+    continue
+  fi
+
+  process "$file"
+done
