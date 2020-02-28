1b9fd1f2d8e5be68d029580c512af933601513f4
Harrison Van <harrison.van@shopback.com>
2020-02-28 08:19:05 +0700

    some ideas

--- scripts/fmt-go-template.sh
+++ scripts/fmt-go-template.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 set -eou pipefail
+
+# Get regex: {{.*}}
+# Replace with one space if found multiple spaces
+
+# Create an open source for this!
+# Using Golang
+
+# Create a Personal GitHub Manager repo
+# Managing all repos from there, using Terraform.
+# Consider using maintenance-cli repo
